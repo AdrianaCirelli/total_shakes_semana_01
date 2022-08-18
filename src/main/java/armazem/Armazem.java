5350221e7307784367236a59f25c21b8052fa264
adrianacirelli <adrianacirelli@ciandt.com>
2022-08-18 15:08:07 -0300

    TDD | Armazem

--- src/main/java/armazem/Armazem.java
+++ src/main/java/armazem/Armazem.java
@@ -2,47 +2,62 @@ package armazem;
 
 import ingredientes.Ingrediente;
 
+import java.util.Map;
 import java.util.TreeMap;
 
 public class Armazem {
+    private Map<Ingrediente, Integer> estoque;
 
-
-    private TreeMap<Ingrediente, Integer> estoque() {
-
-        return this.estoque();
+    public Armazem() {
+        this.estoque = new TreeMap<>();
     }
 
-    public void cadastrarIngredienteEmEstoque(Ingrediente ingrediente) {
+    public void cadastrarIngredienteEmEstoque(Ingrediente ingrediente) throws IllegalArgumentException {
 
-        if(estoque().get(ingrediente) == null){
-            estoque().put(ingrediente, 0);
+        if (!ingredienteExiste(ingrediente)) {
+            estoque.put(ingrediente, 0);
         } else {
             throw new IllegalArgumentException("Ingrediente já cadastrado.");
         }
 
     }
 
-
     public void descadastrarIngredienteEmEstoque(Ingrediente ingrediente) {
-        //TODO
+        if (ingredienteExiste(ingrediente)) {
+            estoque.remove(ingrediente, 0);
+        } else {
+            throw new IllegalArgumentException("Ingrediente nao encontrado.");
+        }
 
+    }
+
+    public boolean ingredienteExiste(Ingrediente ingrediente) {
+
+        return estoque.containsKey(ingrediente);
     }
 
     public void adicionarQuantidadeDoIngredienteEmEstoque(Ingrediente ingrediente, Integer quantidade) {
-        //TODO
-
+        if (ingredienteExiste(ingrediente)) {
+            estoque.put(ingrediente, quantidade);
+        }
+    //TODO
+        // Criar uma validação de quantidade para verificar se é negativo
     }
 
     public void reduzirQuantidadeDoIngredienteEmEstoque(Ingrediente ingrediente, Integer quantidade) {
-        //TODO
-
-    }
-
-    public Integer consultarQuantidadeDoIngredienteEmEstoque(Ingrediente ingrediente){
+        if(ingredienteExiste(ingrediente)){
+            estoque.replace(ingrediente, quantidade, quantidade - quantidade);
+        }
     //TODO
-        return 0;
+        //verificação: o que está em estoque tem que ser maior do que o que está mandando | Verificar se a segunda quantidade é <= a primeira quantidade
     }
 
-    public void cadastrar(Ingrediente aveia) {
+    public int consultarQuantidadeDoIngredienteEmEstoque(Ingrediente ingrediente) {
+
+        if (!ingredienteExiste(ingrediente)) {
+            throw new IllegalArgumentException("Ingrediente nao existe.");
+        }
+        return estoque.get(ingrediente);
     }
+
 }
--- src/test/java/pedido/ArmazemTest.java
+++ src/test/java/pedido/ArmazemTest.java
@@ -1,15 +1,14 @@
 package pedido;
 
 import armazem.Armazem;
-import ingredientes.Ingrediente;
 import ingredientes.TipoTopping;
 import ingredientes.Topping;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
-import static org.junit.Assert.assertEquals;
-clea
+import static org.junit.Assert.*;
+
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class ArmazemTest {
@@ -19,41 +18,87 @@ public class ArmazemTest {
     @BeforeEach
     void setup() throws IllegalArgumentException{
         armazem = new Armazem();
-        Ingrediente ingrediente = new Topping(TipoTopping.Mel);
-        armazem.cadastrar(ingrediente);
-        armazem.adicionarQuantidadeDoIngredienteEmEstoque(ingrediente, 2);
-
+        Topping mel = new Topping(TipoTopping.Mel);
+        //armazem.cadastrarIngredienteEmEstoque(mel);
+        //armazem.adicionarQuantidadeDoIngredienteEmEstoque(mel, 2);
     }
 
     @Test
     public void test_cadastrar_ingrediente_estoque()  {
 
-        Ingrediente ingrediente = new Topping(TipoTopping.Mel);
-        armazem.cadastrar(ingrediente);
-        long quantidade = armazem.consultarQuantidadeDoIngredienteEmEstoque(ingrediente);
+       Topping mel = new Topping(TipoTopping.Mel);
+        armazem.cadastrarIngredienteEmEstoque(mel);
+        long quantidade = armazem.consultarQuantidadeDoIngredienteEmEstoque(mel);
         assertEquals(0, quantidade);;
         
-     }
+    }
+
+    @Test
+    public void test_cadastrar_ingrediente_estoque_ingrediente_existente() throws IllegalArgumentException{
+
+        Throwable exception = assertThrows(IllegalArgumentException.class, () -> {
+              Topping mel = new Topping(TipoTopping.Mel);
+               armazem.cadastrarIngredienteEmEstoque(mel);
+               armazem.cadastrarIngredienteEmEstoque(mel);
+                }
+        );
+
+        assertEquals(exception.getMessage(), "Ingrediente já cadastrado.");
+
+    }
 
     @Test
      public void test_descadastrar_ingrediente_estoque(){
-
+        Topping mel = new Topping(TipoTopping.Mel);
+        armazem.cadastrarIngredienteEmEstoque(mel);
+        armazem.descadastrarIngredienteEmEstoque(mel);
+        assertFalse(armazem.ingredienteExiste(mel));
 
     }
 
 
+
+    @Test
+    public void test_descadastrar_ingrediente_estoque_ingrediente_nao_existente() throws IllegalArgumentException{
+
+        Throwable exception = assertThrows(IllegalArgumentException.class, () -> {
+                    Topping mel = new Topping(TipoTopping.Mel);
+                    armazem.descadastrarIngredienteEmEstoque(mel);
+                }
+        );
+
+        assertEquals(exception.getMessage(), "Ingrediente nao encontrado.");
+
+    }
+
+
+
     @Test
     public void test_adicionar_quantidade_ingrediente_estoque(){
+        Topping mel = new Topping(TipoTopping.Mel);
+        armazem.cadastrarIngredienteEmEstoque(mel);
+        armazem.adicionarQuantidadeDoIngredienteEmEstoque(mel, 2);
+
+        assertEquals(2, armazem.consultarQuantidadeDoIngredienteEmEstoque(mel));
 
     }
 
     @Test
     public void test_reduzir_quantidade_ingrediente_estoque(){
-
+        Topping mel = new Topping(TipoTopping.Mel);
+        armazem.cadastrarIngredienteEmEstoque(mel);
+        armazem.adicionarQuantidadeDoIngredienteEmEstoque(mel, 10);
+        armazem.reduzirQuantidadeDoIngredienteEmEstoque(mel, 10);
+        assertEquals(0, armazem.consultarQuantidadeDoIngredienteEmEstoque(mel));
     }
 
     @Test
     public void test_consultar_quantidade_ingrediente_estoque(){
+        Topping mel = new Topping(TipoTopping.Mel);
+        armazem.cadastrarIngredienteEmEstoque(mel);
+        armazem.adicionarQuantidadeDoIngredienteEmEstoque(mel, 2);
+
+        assertEquals(2, armazem.consultarQuantidadeDoIngredienteEmEstoque(mel));
 
     }
 
